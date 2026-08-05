b52a5526efa8553a2cb9365510e74f16cb480f6a
Philrain <47728948+PhilrainV@users.noreply.github.com>
2026-08-05 13:14:16 +0800

    Update profile.js

--- content/profile.js
+++ content/profile.js
@@ -14,13 +14,13 @@ window.YUANG_WEI_CONTENT.profile = {
   email: "philrain@foxmail.com",
 
   bio: [
-    "I received my Ph.D. in Intelligent Education from the Shanghai Institute of AI for Education, East China Normal University (ECNU), under the supervision of Professor Bo Jiang. I am currently a Lecturer at the Faculty of Artificial Intelligence in Education, Central China Normal University (CCNU). I conduct research on trustworthy and explainable AI for education and have published more than 20 academic papers, including collaborative work.",
-    "I serve as a reviewer for Computers & Education, Education and Information Technologies, Information Processing & Management, IEEE Transactions on Emerging Topics in Computing, International Journal of Artificial Intelligence in Education, Knowledge-Based Systems, and Humanities & Social Sciences Communications, as well as NeurIPS, AAAI, KDD, ICASSP, AIED, and EDM.",
-    "If you are interested in my research, please feel free to contact me. I am always happy to discuss ideas and explore research collaborations.",
+        "I received my Ph.D. in Intelligent Education from the Shanghai Institute of AI for Education at East China Normal University (ECNU), under the supervision of Professor Bo Jiang. I am currently a Lecturer at the Faculty of Artificial Intelligence in Education, Central China Normal University (CCNU). My research focuses on learner modeling, educational large language models, causal modeling, and trustworthy and explainable artificial intelligence in education. I have published more than 20 academic papers.",
+        "I serve as a reviewer for Computers & Education, Education and Information Technologies, Information Processing & Management, IEEE Transactions on Emerging Topics in Computing, the International Journal of Artificial Intelligence in Education, Knowledge-Based Systems, and Humanities & Social Sciences Communications. I have also reviewed submissions for international conferences including NeurIPS, AAAI, KDD, ICASSP, AIED, and EDM.",
+        "If you are interested in my research, please feel free to get in touch. I would be delighted to exchange ideas and explore potential research collaborations.",
   ],
 
   researchInterests: [
-    "AI in Education",
+    "AI for Education",
     "Explainable AI",
     "Causal Models",
     "Knowledge Tracing",
